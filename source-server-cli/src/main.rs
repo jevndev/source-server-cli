123f0dcc2eb2e906fbf6e1606c01bafaa690ad8e
Jonathan Evans <JonathanEvansDev@gmail.com>
2024-09-26 15:58:22 -0500

    Add connection of server

--- source-server-cli/src/main.rs
+++ source-server-cli/src/main.rs
@@ -1,12 +1,36 @@
+use std::process::ExitCode;
+
 use clap::Parser;
+use source_query_protocol::ServerAddress;
 
 mod command_line_interface;
 mod source_query_protocol;
 
-fn main() {
+fn main() -> ExitCode {
     let arguments = command_line_interface::Cli::parse();
     println!(
         "{}, {}, {:?}",
         arguments.ip, arguments.port, arguments.command
-    )
+    );
+
+    let server_connection = source_query_protocol::ServerConnection::connect(
+        ServerAddress::from_ipstr(arguments.ip, arguments.port),
+    );
+
+    if !server_connection.is_ok() {
+        println!("Failed to connect to server");
+        // print what went wrong
+        println!("{}", server_connection.err().unwrap());
+        return ExitCode::FAILURE;
+    }
+
+    println!("Connected to server");
+
+    match arguments.command {
+        command_line_interface::QueryCommand::Players => {
+            println!("Querying players");
+        }
+    }
+
+    return ExitCode::SUCCESS;
 }
--- source-server-cli/src/source_query_protocol/mod.rs
+++ source-server-cli/src/source_query_protocol/mod.rs
@@ -11,7 +11,8 @@ pub struct ServerConnection {
 impl ServerConnection {
     pub fn connect(server_address: ServerAddress) -> Result<ServerConnection, std::io::Error> {
         let socket_addr = SocketAddr::new(server_address.address, server_address.port);
-        let socket = UdpSocket::bind(socket_addr)?;
+        let socket = UdpSocket::bind("0.0.0.0:0")?;
+        socket.connect(socket_addr)?;
 
         return Ok(ServerConnection { socket });
     }
@@ -20,4 +21,14 @@ impl ServerConnection {
 pub struct ServerAddress {
     address: IpAddr,
     port: u16,
+}
+
+impl ServerAddress {
+    pub fn from_ipstr(ip: String, port: u16) -> ServerAddress {
+        let ip_addr: IpAddr = ip.parse().expect("Invalid IP address format");
+        return ServerAddress {
+            address: ip_addr,
+            port: port,
+        };
+    }
 }
